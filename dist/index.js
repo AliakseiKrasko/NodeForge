0bf4f67338cee79ec598e8fdbb5607b01fc1cf2d
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-04-02 20:41:54 +0300

    dffgvxsfddgfsgb

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    let helloMessadge = 'Hello World';
+    let helloMessadge = 'Hello World!!орпорпорпо!!!';
     res.send(helloMessadge);
 });
 app.listen(port, () => {
